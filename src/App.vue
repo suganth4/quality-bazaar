8a5f76c45497376328c2cdeddfd1a84d0ed3e696
suganth4 <suganthsuganth6@gmail.com>
2022-03-01 12:28:13 +0530

    --- Auto-Git Commit ---

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  {{ count }}
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios'
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data: ()=> {
+   return{ count:0
+   }
+  },
+  mounted:  async function () {
+    axios.get(''https://jsonplaceholder.typicode.com/todos/1']')
+    .then((response) => {
+      console.log(response);
+    }).catch(()=>{
+      console.log("sdfsd");
+    })
   }
 }
 </script>
