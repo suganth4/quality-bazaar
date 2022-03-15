8344ed6997aa5f6369e95b24c7a7f030398f1df9
suganth4 <suganthsuganth6@gmail.com>
2022-03-15 22:24:37 +0530

    --- Auto-Git Commit ---

--- src/components/login-qa.vue
+++ src/components/login-qa.vue
@@ -1,20 +1,23 @@
 <template>
     <div>
         <div id="header">
-            <div id="option">
-                <a href="" >MEN</a>    
-                <a href="" >WOMEN</a>    
-                <a href="" >KIDS</a>    
-                <a href="" >BEAUTY</a>    
-                <a href="" >STUDIO</a>    
+            <div>
+                <img src="../assets/myntra.jpg" alt="" id="image">
+            </div>
+            <div class="option">
+                <a href="" class="options-list">MEN</a>    
+                <a href="" class="options-list">WOMEN</a>    
+                <a href="" class="options-list">KIDS</a>    
+                <a href="" class="options-list">BEAUTY</a>    
+                <a href="" class="options-list">STUDIO</a>    
             </div>
             <div>
-                <input type="text" name="" value="">
+                <input type="text" name="" value="" id="search">
             </div>
-            <div>
-            <a href="">Profile</a>
-            <a href="">Wishlist</a>
-            <a href="">Bag</a>
+            <div class="option">
+                <a href="" class="kart">Profile</a>
+                <a href="" class="kart">Wishlist</a>
+                <a href="" class="kart">Bag</a>
             </div>
         </div>
     </div>
@@ -28,10 +31,22 @@ export default {
 </script>
 
 <style>
-#header, #option {
+#header, .option {
     display: flex;
-    justify-content: space-around ;
+    justify-content: space-between ;
 }
-
-
+.options-list, .kart {
+    text-decoration: none;
+    color: black;
+    font-size: 15px;
+    margin: 10px;
+}
+#search {
+    font-size: 20px;
+    margin: 10px;
+}
+#image {
+    width: 150px;
+    height: 150px;
+}
 </style>
