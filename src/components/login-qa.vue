0748a85007626e58c9c49354ddf66d093ec8638e
suganth4 <suganthsuganth6@gmail.com>
2022-03-23 21:09:50 +0530

    --- Auto-Git Commit ---

--- src/components/login-qa.vue
+++ src/components/login-qa.vue
@@ -1,4 +1,19 @@
 <template>
+<div class="container">
+  <h2>Dropdowns</h2>
+  <p>The .dropdown class is used to indicate a dropdown menu.</p>
+  <p>Use the .dropdown-menu class to actually build the dropdown menu.</p>
+  <p>To open the dropdown menu, use a button or a link with a class of .dropdown-toggle and data-toggle="dropdown".</p>                                          
+  <div class="dropdown">
+    <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Dropdown Example
+    <span class="caret"></span></button>
+    <ul class="dropdown-menu">
+      <li><a href="#">HTML</a></li>
+      <li><a href="#">CSS</a></li>
+      <li><a href="#">JavaScript</a></li>
+    </ul>
+  </div>
+</div>
     <div>
         <div id="header">
             <div class="option">
@@ -8,8 +23,15 @@
                 <a href="" class="options-list">BEAUTY</a>    
                 <a href="" class="options-list">STUDIO</a>    
             </div>
-            <ul>
-                <li></li>
+            <ul id="men">
+                <li>suganth</li>
+                <li>suganth</li>
+                <li>suganth</li>
+                <li>suganth</li>
+                <li>suganth</li>
+                <li>suganth</li>
+                <li>suganth</li>
+                <li>suganth</li>
             </ul>
             <div>
                 <input type="text" name="" value="" placeholder="Search for products, brands and more"  id="search">
