37c89ebac658be7f3f5e2461fd5a26ca5a106130
suganth4 <suganthsuganth6@gmail.com>
2022-05-01 18:04:56 +0530

    --- Auto-Git Commit ---

---
+++ src/data/dressInfo.js
@@ -0,0 +1,28 @@
+export const men =  {
+    Men : [
+        {
+            title: "Topwear",
+            categories: ["T-Shirts", "Casual Shirts", "Formal Shirts", "Sweatshirts", "Sweaters", "Jackets", "Blazers & Coats", "Suits", "Rain Jackets"]
+        },
+        {
+            title: "Indian & Festive Wear",
+            categories: ["Kurtas & Kurta Sets", "Sherwanis", "Nehru Jackets", "Dhotis"]
+        },
+        {
+            title: "Bottomwear",
+            categories: ["Jeans", "Casual Trousers", "Formal Trousers", "Shorts", "Track Pants & Joggers"]
+        },
+        {
+            title: "Innerwear & Sleepwear",
+            categories: ["Briefs & Trunks", "Boxers", "Vests", "Sleepwear & Loungewear", "Thermals"]
+        },
+        {
+            title: "Plus Size",
+            categories: []
+        },
+        {
+            title: "Footwear",
+            
+        }
+    ],
+};
